28cd909f7f7e644388e7d9fe4bed79a9c2226ff1
Alexander Fields <af03848@georgiasouthern.edu>
2020-10-14 19:54:24 -0400

    Update Form1.Designer.cs

--- myRSA/Form1.Designer.cs
+++ myRSA/Form1.Designer.cs
@@ -1,5 +1,10 @@
 ﻿namespace myRSA
 {
+     /**
+     Code created by Alexander Fields https://github.com/roku674  || https://www.linkedin.com/in/alexander-fields-aa57a997/
+feel free to donate https://sites.google.com/view/perilousgamesltd/donate
+Brave is a save browser that doesn't track your data unless you want it to be tracked. You can then be paied (in cryptocurrency, but still paid nonetheless) for your data https://brave.com/rok079
+     **/
     partial class Form1
     {
         /// <summary>
