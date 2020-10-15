fa934cd59247233d3d887a2f46ebeca15fc92cb3
roku674 <af03848@georgiasouthern.edu>
2020-10-14 21:08:56 -0400

    changes to comments

--- myRSA/Form1.cs
+++ myRSA/Form1.cs
@@ -14,7 +14,7 @@ namespace myRSA
     /**
      Code created by Alexander Fields https://github.com/roku674  || https://www.linkedin.com/in/alexander-fields-aa57a997/
 feel free to donate https://sites.google.com/view/perilousgamesltd/donate
-Brave is a save browser that doesn't track your data unless you want it to be tracked. You can then be paied (in cryptocurrency, but still paid nonetheless) for your data https://brave.com/rok079
+Brave is a safe browser that doesn't track your data unless you want it to be tracked. You can then be paid (in cryptocurrency, but still paid nonetheless) for your data https://brave.com/rok079
      **/
     public partial class Form1 : Form
     {
